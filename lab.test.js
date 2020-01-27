007ac73f227be27932141d54b32533b9afb54d5c
Francis Aboud <francisaboud2000@gmail.com>
2020-01-27 17:52:00 +0200

    adds new test

--- lab.test.js
+++ lab.test.js
@@ -1,3 +1,9 @@
 test('adds 1 + 2 to equal 3', () => {
     expect(1+2).toBe(3);
   });
+  test('adds 1 + 2 to equal 3', () => {
+    expect(4+5).toBe(9);
+  });
+  test('adds 1 + 2 to equal 3 wrong', () => {
+    expect(4+5).toBe(2);
+  });
